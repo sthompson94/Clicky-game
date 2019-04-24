0d3f9c982781290f6665f629c5aea11528bb3cf1
Sam Thompson <electronic62794@yahoo.com>
2019-04-23 22:32:54 -0700

    made a Nav component

---
+++ clicky-game/src/components/title.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Nav() {
+    return (
+      <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+        <h1 className="navbar-brand"> Clicky Game</h1>
+      </nav>
+    );
+  }
+
+  export default Nav;
