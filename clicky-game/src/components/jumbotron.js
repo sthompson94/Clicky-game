ee6c2fa55c0abaddbfe0e9ce3175799eb318a211
Sam Thompson <electronic62794@yahoo.com>
2019-04-25 13:55:45 -0700

    changed theme to be a little more evil, game still functions as expected

--- clicky-game/src/components/jumbotron.js
+++ clicky-game/src/components/jumbotron.js
@@ -3,8 +3,8 @@ import React from 'react'
 function Jumbotron() {
     return(
     <div className="jumbotron">
-    <h1 className="display-4">Instructions!</h1>
-    <p className="lead">Try to click all 12 cartoons! but don't click the same one twice!</p>
+    <h1 className="display-4">Kristen Stewart's Emotions</h1>
+    <p className="lead">Each picture shuffles. don't click the same one twice!</p>
   </div>
 
     )
--- clicky-game/src/data/data.js
+++ clicky-game/src/data/data.js
@@ -1,50 +1,50 @@
 const characters = [{
-        name: "Tina Belcher",
-        src: "https://i.imgur.com/wS9Sg2C.png"
+        name: "Happy",
+        src: "https://i.imgur.com/70hFZWD.jpg"
     },
     {
-        name: "Spongebob",
-        src: "https://i.imgur.com/Ij9ef5s.jpg"
+        name: "Sad",
+        src: "https://i.imgur.com/70hFZWD.jpg"
     },
     {
-        name: "Dexter",
-        src: "https://i.imgur.com/Py9HxeX.jpg"
+        name: "Anxious",
+        src: "https://i.imgur.com/70hFZWD.jpg"
     },
     {
-        name: "Power Puff Girls",
-        src: "https://i.imgur.com/kJp3GuN.jpg"
+        name: "Angry",
+        src: "https://i.imgur.com/70hFZWD.jpg"
     },
     {
-        name: "Ed",
-        src: "https://i.imgur.com/hL0uXqN.jpg?1"
+        name: "Hurt",
+        src: "https://i.imgur.com/70hFZWD.jpg"
     },
     {
-        name: "Courage",
-        src: "https://i.imgur.com/gIetVdY.jpg"
+        name: "Delighted",
+        src: "https://i.imgur.com/70hFZWD.jpg"
     },
     {
-        name: "Danny Phantom",
-        src: "https://i.imgur.com/0pH65uk.jpg"
+        name: "Depressed",
+        src: "https://i.imgur.com/70hFZWD.jpg"
     },
     {
-        name: "Rugrats",
-        src: "https://i.imgur.com/m1pBFpT.jpg"
+        name: "Calm",
+        src: "https://i.imgur.com/70hFZWD.jpg"
     },
     {
-        name: "Cat-Dog",
-        src: "https://i.imgur.com/EtWGTVs.png"
+        name: "Joy",
+        src: "https://i.imgur.com/70hFZWD.jpg"
     },
     {
-        name: "Timmy Turner",
-        src: "https://i.imgur.com/us5fOfG.jpg"
+        name: "Interested",
+        src: "https://i.imgur.com/70hFZWD.jpg"
     },
     {
-        name: "Hey Arnold",
-        src: "https://i.imgur.com/KLBifrA.png"
+        name: "Fear",
+        src: "https://i.imgur.com/70hFZWD.jpg"
     },
     {
-        name: "Cow and Chicken",
-        src: "https://i.imgur.com/0LC6zUf.jpg"
+        name: "Bored",
+        src: "https://i.imgur.com/70hFZWD.jpg"
     }
 ]
 
